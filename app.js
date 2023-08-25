17e67e37a3547d46ad5a5e3c2d160a7f017c81e1
ealikan <ethem_alikan@hotmail.com>
2023-08-25 13:09:27 +0300

    ejs eklendi

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs")
+
+//Template engine
+app.set("view engine","ejs")
+
+//middlewares
+app.use(express.static("public"))
 
 app.get("/", function (req, res) {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("index");
 });
+app.get("/add_post", function (req, res) {
+  res.render("add_post");
+});
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
 
 app.listen(3000);
